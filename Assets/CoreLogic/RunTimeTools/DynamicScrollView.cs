514bac5d7fc8db8b77978a47e8bc42c7e7515b24
Ajieloa <1758580256@qq.com>
2024-08-06 01:07:33 +0800

    perf：刷新

--- Assets/CoreLogic/RunTimeTools/DynamicScrollView.cs
+++ Assets/CoreLogic/RunTimeTools/DynamicScrollView.cs
@@ -16,6 +16,8 @@ public class DynamicScrollView : UIBehaviour, IInitializePotentialDragHandler, I
     private GameObject mElement = null;
     public GameObject Element { get => mElement; set => mElement = value; }
 
+    public Vector2 mElementSize = Vector2.zero;
+
     [SerializeField]
     private int mElementCount = 0;
     public int ElementCount { get => mElementCount; set => mElementCount = value; }
@@ -30,7 +32,6 @@ public class DynamicScrollView : UIBehaviour, IInitializePotentialDragHandler, I
     [SerializeField]
     private bool mIsVertical = true;
     public bool IsVertical { get => mIsVertical; set => mIsVertical = value; }
-
     [SerializeField]
     private Vector2 mOffsetCount = Vector2.zero;
     public Vector2 OffsetCount { get => mOffsetCount; set => mOffsetCount = value; }
@@ -59,8 +60,10 @@ public class DynamicScrollView : UIBehaviour, IInitializePotentialDragHandler, I
     public bool IsScroll { get => mIsScroll; set => mIsScroll = value; }
 
     public RectTransform mContent = null;
+    public RectTransform mViewport = null;
     public Scrollbar mHorizontal = null;
     public Scrollbar mVertical = null;
+    private RectTransform mScroll = null;
 
     public Vector2 mInitMouseVec = Vector2.zero;
     public Vector2 mCurMouseVec = Vector2.zero;
@@ -69,13 +72,49 @@ public class DynamicScrollView : UIBehaviour, IInitializePotentialDragHandler, I
     private float mInitH = 0.0f;
     private float mInitV = 0.0f;
     public Vector2 mContentSize = Vector2.zero;
+    public Vector2 mShowSize = Vector2.zero;
     protected override void Awake()
     {
+        mVertical?.onValueChanged.AddListener((_value) =>
+        {
+            OnUpdateContentByVertical();
+        });
+        mHorizontal?.onValueChanged.AddListener((_value) =>
+        {
+            OnUpdateContentByHorizontal();
+        });
         mContentSize.y += mMargin.x;
         mContentSize.y += mMargin.y;
-        mContentSize.y += (mElementCount * (mElement.transform as RectTransform).sizeDelta.y);
+        mElementSize = (mElement.transform as RectTransform).sizeDelta;
+        mContentSize.y += (mElementCount * mElementSize.y);
         mContentSize.y += (mElementCount - 1) * mOffset.y;
+        mScroll = transform.GetComponent<RectTransform>();
+        mShowSize = new Vector2(mScroll.sizeDelta.x + mViewport.sizeDelta.x, mScroll.sizeDelta.y + mViewport.sizeDelta.y);
 
+    }
+    protected virtual void OnUpdateVerticalSlider()
+    {
+        if (mVertical == null)
+            return;
+        mVertical.SetValueWithoutNotify(mContent.anchoredPosition.y / (float)(mContentSize.y - mShowSize.y));
+    }
+    public virtual void OnUpdateHorizontalSlider()
+    {
+        if (mHorizontal == null)
+            return;
+        mHorizontal.SetValueWithoutNotify(mContent.anchoredPosition.x / (float)(mContentSize.x - mShowSize.x));
+    }
+    public virtual void OnUpdateContentByVertical()
+    {
+        if (mVertical == null)
+            return;
+        mContent.anchoredPosition = new Vector2(mContent.anchoredPosition.x, Mathf.Clamp((mVertical.value * (mContentSize.y - mShowSize.y)), 0, mContentSize.y));
+    }
+    public virtual void OnUpdateContentByHorizontal()
+    {
+        if (mHorizontal == null)
+            return;
+        mContent.anchoredPosition = new Vector2(Mathf.Clamp((mHorizontal.value * (mContentSize.x - mShowSize.x)), 0, mContentSize.x), mContent.anchoredPosition.y);
     }
     protected override void Start()
     {
@@ -91,16 +130,34 @@ public class DynamicScrollView : UIBehaviour, IInitializePotentialDragHandler, I
                 ElementInfo _elemet = new ElementInfo();
                 _elemet.Element = _gam;
                 _elemet.Script = _gam.GetComponent<IDynamicScrollViewRefresh>();
-                if (i < mElementCount)
+                mHeadIndex = 0;
+                if (i < 4)
                 {
                     UsePool.Add(_elemet);
-                    _elemet.Element.transform.position = new Vector2(0, (-Margin.x - i * 50));
+                    _elemet.Rect.anchoredPosition = new Vector2(0, (-Margin.x - i * mElementSize.y));
                     _elemet.Script.OnRefreshByIndex(i);
+                    mEndIndex = i;
 
                 }
                 else
                     mPool.Add(_elemet);
             }
+    }
+    private int mHeadIndex = 0;
+    private int mEndIndex = 0;
+    public virtual void OnUpdateContentElement()
+    {
+        if (mContent.anchoredPosition.y > mInitV)
+        {
+            if (Mathf.CeilToInt((mContent.anchoredPosition.y - mInitV) / 30) < 1)
+            {
+
+            }
+        }
+        else if (mContent.anchoredPosition.y < mInitV)
+        {
+
+        }
     }
 
     public virtual void LateUpdate()
@@ -113,20 +170,17 @@ public class DynamicScrollView : UIBehaviour, IInitializePotentialDragHandler, I
         mInitMouseVec = eventData.position;
         mInitH = mContent.anchoredPosition.x;
         mInitV = mContent.anchoredPosition.y;
-        LogMgr.Instance.CWarn("OnInitializePotentialDrag");
     }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
         mCurMouseVec = eventData.position;
         mIsOnDrag = true;
-        LogMgr.Instance.CWarn("OnBeginDrag");
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
         mIsOnDrag = false;
-        LogMgr.Instance.CWarn("OnEndDrag");
     }
 
     public void OnDrag(PointerEventData eventData)
@@ -138,10 +192,17 @@ public class DynamicScrollView : UIBehaviour, IInitializePotentialDragHandler, I
             return;
         Vector2 _vec = new Vector2(0, 0);
         if (mIsHorizontal)
+        {
             _vec.x = Mathf.Clamp((mInitH + mCurMouseVec.x - mInitMouseVec.x), 0, mContent.sizeDelta.x);
+            OnUpdateHorizontalSlider();
+        }
         if (mIsVertical)
+        {
             _vec.y = Mathf.Clamp((mInitV + mCurMouseVec.y - mInitMouseVec.y), 0, (mContentSize.y - (transform as RectTransform).sizeDelta.y));
+            OnUpdateVerticalSlider();
+        }
         mContent.anchoredPosition = _vec;
+        OnUpdateContentElement();
 
     }
 
@@ -149,29 +210,40 @@ public class DynamicScrollView : UIBehaviour, IInitializePotentialDragHandler, I
     {
         if (mIsScroll)
             return;
-        LogMgr.Instance.CWarn($"OnScroll:{eventData.scrollDelta}");
 
     }
 
     public void Rebuild(CanvasUpdate executing)
     {
-        LogMgr.Instance.CWarn("Rebuild");
     }
 
     public void LayoutComplete()
     {
-        LogMgr.Instance.CWarn("LayoutComplete");
     }
 
     public void GraphicUpdateComplete()
     {
-        LogMgr.Instance.CWarn("GraphicUpdateComplete");
+    }
+    public virtual void OnClear()
+    {
+        mVertical?.onValueChanged.RemoveAllListeners();
+        mHorizontal?.onValueChanged.RemoveAllListeners();
     }
 }
 public struct ElementInfo
 {
     private GameObject mElement;
-    public GameObject Element { get => mElement; set => mElement = value; }
+    public GameObject Element
+    {
+        get => mElement;
+        set
+        {
+            mElement = value;
+            mRectTransform = mElement.GetComponent<RectTransform>();
+        }
+    }
+    private RectTransform mRectTransform;
+    public RectTransform Rect { get => mRectTransform; }
 
     private IDynamicScrollViewRefresh mScript;
     public IDynamicScrollViewRefresh Script { get => mScript; set => mScript = value; }
